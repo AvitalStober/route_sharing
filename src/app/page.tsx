c8520cb327a3f066855d2de06107580cd6a6932e
Chani Cohen <72072ch@gmail.com>
2024-12-11 13:21:48 +0200

    route login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import "./globals.css";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  router.push("/pages/login");
+  return(
+    <></>
+  )
 }
