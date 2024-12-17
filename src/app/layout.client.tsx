a77aec9a21ea60373f6af0c85b2f5a39288a3ba8
AvitalStober <avitals5050@gmail.com>
2024-12-17 13:25:27 +0200

    merge log out

---
+++ src/app/layout.client.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { usePathname, useRouter } from "next/navigation";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const router = useRouter();
+  const pathname = usePathname(); // גישה לנתיב הנוכחי
+  if (typeof window === "undefined") {
+    return null;
+  }
+  const userTokenFromStorage = localStorage.getItem("userToken");
+  
+  // בדיקה אם המשתמש לא מחובר ומנסה לגשת לדפים לא מורשים
+  const publicPaths = ["/login", "/signup", "/forgot-password", "/noAccess"];
+  if (!userTokenFromStorage && !publicPaths.some(path => pathname.includes(path))) {
+    router.push("/pages/noAccess");
+  }
+
+  return <>{children}</>;
+}
