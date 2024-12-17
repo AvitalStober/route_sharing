da7efa00d2e510e267faaa9c6e4f2c195eac53df
AvitalStober <avitals5050@gmail.com>
2024-12-17 11:18:46 +0200

    fix initialize

--- src/app/api/routes/[routeId]/route.ts
+++ src/app/api/routes/[routeId]/route.ts
@@ -2,15 +2,33 @@ import connect from "@/app/lib/DB/connectDB";
 import Route from "@/app/lib/models/routeModel";
 import { NextResponse } from "next/server";
 
+// export async function GET(
+//   request: Request,
+//   { params }: { params: { routeId: string } }
+// ) {
+//   try {
+//     await connect();
+//     const { routeId } = await params;
+//     const routes = await Route.find({ _id: routeId });
+//     return NextResponse.json(routes, { status: 200 });
+//   } catch (error) {
+//     console.error("Error fetching routes:", error);
+//     return NextResponse.json(
+//       { error: "Internal Server Error" },
+//       { status: 500 }
+//     );
+//   }
+// }
+
 export async function GET(
   request: Request,
-  { params }: { params: { routeId: string } }
+  context: { params: { routeId: string } }
 ) {
   try {
     await connect();
-    const { routeId } = await params;
-    const route = await Route.findById(routeId);
-    return NextResponse.json(route, { status: 200 });
+    const { routeId } = context.params; // צריך לגשת דרך 'context.params'
+    const routes = await Route.find({ _id: routeId });
+    return NextResponse.json(routes, { status: 200 });
   } catch (error) {
     console.error("Error fetching routes:", error);
     return NextResponse.json(
@@ -19,6 +37,7 @@ export async function GET(
     );
   }
 }
+
 
 export async function PUT(
   request: Request,
@@ -32,20 +51,22 @@ export async function PUT(
     console.log(routeId, "id", newRate, "rate", gallery, "gallery");
 
     let updatedRoute;
-
+    
     // Check if newRate is provided and calculate new rating
     if (newRate) {
       const route = await Route.findById(routeId);
       if (!route) {
-        return NextResponse.json({ error: "Route not found" }, { status: 404 });
+        return NextResponse.json(
+          { error: "Route not found" },
+          { status: 404 }
+        );
       }
 
       // Calculate the new average rate and increment rating number
-      const newUpdateRate =
-        (route.rate * route.ratingNum + newRate) / (route.ratingNum + 1);
+      const newUpdateRate = (route.rate * route.ratingNum + newRate) / (route.ratingNum + 1);
       route.rate = newUpdateRate;
       route.ratingNum = route.ratingNum + 1;
-
+      
       updatedRoute = await route.save(); // Save the updated route
     }
 
