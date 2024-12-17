abd44e14993419e11eb26152be75773d76c52d3c
AvitalStober <avitals5050@gmail.com>
2024-12-17 13:43:43 +0200

    merge log out

--- src/app/api/routes/[routeId]/route.ts
+++ src/app/api/routes/[routeId]/route.ts
@@ -1,30 +1,25 @@
-// import connect from "@/app/lib/DB/connectDB";
-// import User from "@/app/lib/models/userModel";
-// import { NextResponse } from "next/server";
+import connect from "@/app/lib/DB/connectDB";
+import Route from "@/app/lib/models/routeModel";
+import { NextResponse } from "next/server";
 
-export async function GET() {
-  console.log("get");
-  
+export async function GET(
+  request: Request,
+  { params }: { params: { routeId: string } }
+) {
+  try {
+    await connect();
+    const { routeId } = await params;
+
+    const routes = await Route.find({ _id: routeId });
+    return NextResponse.json(routes, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching routes:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
-
-// export async function GET(
-//   request: Request,
-//   { params }: { params: { routeId: string } }
-// ) {
-//   try {
-//     await connect();
-//     const { routeId } = await params;
-
-//     const routes = await Route.find({ _id: routeId });
-//     return NextResponse.json(routes, { status: 200 });
-//   } catch (error) {
-//     console.error("Error fetching routes:", error);
-//     return NextResponse.json(
-//       { error: "Internal Server Error" },
-//       { status: 500 }
-//     );
-//   }
-// }
 
 // import connect from "@/app/lib/DB/connectDB";
 // import Route from "@/app/lib/models/routeModel";
@@ -60,60 +55,60 @@ export async function GET() {
 //   }
 // }
 
-// export async function PUT(
-//   request: Request,
-//   { params }: { params: { routeId: string } }
-// ) {
-//   try {
-//     await connect();
+export async function PUT(
+  request: Request,
+  { params }: { params: { routeId: string } }
+) {
+  try {
+    await connect();
 
-//     const { routeId } = params;
-//     const { rate: newRate, gallery } = await request.json();
-//     console.log(routeId, "id", newRate, "rate", gallery, "gallery");
+    const { routeId } = params;
+    const { rate: newRate, gallery } = await request.json();
+    console.log(routeId, "id", newRate, "rate", gallery, "gallery");
 
-//     let updatedRoute;
+    let updatedRoute;
 
-//     // Check if newRate is provided and calculate new rating
-//     if (newRate) {
-//       const route = await Route.findById(routeId);
-//       if (!route) {
-//         return NextResponse.json({ error: "Route not found" }, { status: 404 });
-//       }
+    // Check if newRate is provided and calculate new rating
+    if (newRate) {
+      const route = await Route.findById(routeId);
+      if (!route) {
+        return NextResponse.json({ error: "Route not found" }, { status: 404 });
+      }
 
-//       // Calculate the new average rate and increment rating number
-//       const newUpdateRate =
-//         (route.rate * route.ratingNum + newRate) / (route.ratingNum + 1);
-//       route.rate = newUpdateRate;
-//       route.ratingNum = route.ratingNum + 1;
+      // Calculate the new average rate and increment rating number
+      const newUpdateRate =
+        (route.rate * route.ratingNum + newRate) / (route.ratingNum + 1);
+      route.rate = newUpdateRate;
+      route.ratingNum = route.ratingNum + 1;
 
-//       updatedRoute = await route.save(); // Save the updated route
-//     }
+      updatedRoute = await route.save(); // Save the updated route
+    }
 
-//     // Update gallery if provided
-//     if (gallery) {
-//       updatedRoute = await Route.findByIdAndUpdate(
-//         routeId,
-//         { $set: { gallery: gallery } },
-//         { new: true }
-//       );
-//     }
+    // Update gallery if provided
+    if (gallery) {
+      updatedRoute = await Route.findByIdAndUpdate(
+        routeId,
+        { $set: { gallery: gallery } },
+        { new: true }
+      );
+    }
 
-//     if (updatedRoute) {
-//       return NextResponse.json(updatedRoute, { status: 200 });
-//     } else {
-//       return NextResponse.json(
-//         { error: "Route not found or no updates made" },
-//         { status: 404 }
-//       );
-//     }
-//   } catch (error) {
-//     console.error("Error updating route:", error);
-//     return NextResponse.json(
-//       { error: "Internal Server Error" },
-//       { status: 500 }
-//     );
-//   }
-// }
+    if (updatedRoute) {
+      return NextResponse.json(updatedRoute, { status: 200 });
+    } else {
+      return NextResponse.json(
+        { error: "Route not found or no updates made" },
+        { status: 404 }
+      );
+    }
+  } catch (error) {
+    console.error("Error updating route:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
 
 // export async function PUT(
 //   request: Request,
