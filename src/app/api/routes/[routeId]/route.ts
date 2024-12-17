2c38210822c73d9a83e7f8551010e21d40946a77
AvitalStober <avitals5050@gmail.com>
2024-12-17 11:15:59 +0200

    fix initialize

--- src/app/api/routes/[routeId]/route.ts
+++ src/app/api/routes/[routeId]/route.ts
@@ -4,13 +4,13 @@ import { NextResponse } from "next/server";
 
 export async function GET(
   request: Request,
-  context: { params: { routeId: string } }
+  { params }: { params: { routeId: string } }
 ) {
   try {
     await connect();
-    const { routeId } = await context.params;
-    const routes = await Route.find({ _id: routeId });
-    return NextResponse.json(routes, { status: 200 });
+    const { routeId } = await params;
+    const route = await Route.findById(routeId);
+    return NextResponse.json(route, { status: 200 });
   } catch (error) {
     console.error("Error fetching routes:", error);
     return NextResponse.json(
@@ -32,22 +32,20 @@ export async function PUT(
     console.log(routeId, "id", newRate, "rate", gallery, "gallery");
 
     let updatedRoute;
-    
+
     // Check if newRate is provided and calculate new rating
     if (newRate) {
       const route = await Route.findById(routeId);
       if (!route) {
-        return NextResponse.json(
-          { error: "Route not found" },
-          { status: 404 }
-        );
+        return NextResponse.json({ error: "Route not found" }, { status: 404 });
       }
 
       // Calculate the new average rate and increment rating number
-      const newUpdateRate = (route.rate * route.ratingNum + newRate) / (route.ratingNum + 1);
+      const newUpdateRate =
+        (route.rate * route.ratingNum + newRate) / (route.ratingNum + 1);
       route.rate = newUpdateRate;
       route.ratingNum = route.ratingNum + 1;
-      
+
       updatedRoute = await route.save(); // Save the updated route
     }
 
