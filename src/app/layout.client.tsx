fbda9b5e888faea4642c6b21a2ec0c72e19d1824
YaelNagar <165303327+YaelNagar@users.noreply.github.com>
2024-12-19 14:28:51 +0200

    merge project branch

--- src/app/layout.client.tsx
+++ src/app/layout.client.tsx
@@ -1,24 +1,24 @@
-'use client'
-import { usePathname, useRouter } from "next/navigation";
-import "./globals.css";
+// 'use client'
+// import { usePathname, useRouter } from "next/navigation";
+// import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  const router = useRouter();
-  const pathname = usePathname(); // גישה לנתיב הנוכחי
-  if (typeof window === "undefined") {
-    return null;
-  }
-  const userTokenFromStorage = localStorage.getItem("userToken");
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   const router = useRouter();
+//   const pathname = usePathname(); // גישה לנתיב הנוכחי
+//   if (typeof window === "undefined") {
+//     return null;
+//   }
+//   const userTokenFromStorage = localStorage.getItem("userToken");
   
-  // בדיקה אם המשתמש לא מחובר ומנסה לגשת לדפים לא מורשים
-  const publicPaths = ["/login", "/signup", "/forgot-password", "/noAccess"];
-  if (!userTokenFromStorage && !publicPaths.some(path => pathname.includes(path))) {
-    router.push("/pages/noAccess");
-  }
+//   // בדיקה אם המשתמש לא מחובר ומנסה לגשת לדפים לא מורשים
+//   const publicPaths = ["/login", "/signup", "/forgot-password", "/noAccess"];
+//   if (!userTokenFromStorage && !publicPaths.some(path => pathname.includes(path))) {
+//     router.push("/pages/noAccess");
+//   }
 
-  return <>{children}</>;
-}
+//   return <>{children}</>;
+// }
