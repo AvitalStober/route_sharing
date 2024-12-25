522fdaba3a0c7f59c440cf0e0d2c896e20fd4c7e
Chani Cohen <72072ch@gmail.com>
2024-12-25 12:20:10 +0200

    style popup galery

--- src/app/components/CardMap.tsx
+++ src/app/components/CardMap.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleMap, DirectionsRenderer } from "@react-google-maps/api";
 import PopUpRoute from "./PopUpRoute";
 import { Types } from "mongoose";
 import { CardMapProps } from "../types/props/CardMapProps";
+import { log } from "node:console";
 
 const CardMap: React.FC<CardMapProps> = ({
   points = [], // נותנים ערך ברירת מחדל ריק למערך
@@ -29,7 +30,8 @@ const CardMap: React.FC<CardMapProps> = ({
   const center = points.length > 0 ? points[0] : { lat: 0, lng: 0 };
 
   const calculateRoute = () => {
-    // אם אין מספיק נקודות למסלול
+    console.log("enter");
+
     if (points.length < 2) {
       alert("עליך לבחור לפחות שתי נקודות למסלול.");
       return;
@@ -45,18 +47,19 @@ const CardMap: React.FC<CardMapProps> = ({
       })),
       travelMode: google.maps.TravelMode.WALKING,
     };
-
+    debugger
     directionsService.route(request, (result, status) => {
       if (status === google.maps.DirectionsStatus.OK && result) {
         calculateWalkingTime(result);
         setDirections(result);
       } else {
-        alert("לא ניתן לחשב מסלול.");
+        alert("לא ניתן לחשב מסלול");
       }
     });
   };
 
   const calculateWalkingTime = (result: google.maps.DirectionsResult) => {
+
     let totalTimeInSeconds = 0;
 
     const route = result.routes[0];
@@ -64,7 +67,7 @@ const CardMap: React.FC<CardMapProps> = ({
     route.legs.forEach((leg) => {
       totalTimeInSeconds += leg.duration!.value;
     });
-
+    debugger
     const calculatedHours = Math.floor(totalTimeInSeconds / 3600); // שעות
     const calculatedMinutes = Math.floor((totalTimeInSeconds % 3600) / 60); // דקות
 
@@ -83,25 +86,30 @@ const CardMap: React.FC<CardMapProps> = ({
     fullscreenControl: true, // מבטל את כפתור המסך המלא
   };
 
+  useEffect(() => {
+    calculateRoute();
+  }, [minutes]);
+
   return (
     <>
       <div className="flex justify-center">
         {!isExpanded && !expanded && (
-          <button
-            onClick={() => setIsExpanded(true)}
-            className="my-2 px-4 py-2 border-slate-700 text-slate-700 font-medium text-sm rounded-lg shadow hover:border-slate-700 hover:shadow-lg transition duration-300"
-            // className="m-2 rounded-md py-2 px-4 border border-transparent text-center text-sm text-slate-700 transition-all shadow hover:shadow-lg focus:border-slate-700 focus:shadow-none active:border-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            type="button"
-          >
-            ראה עוד
-          </button>
+          <>
+            <button
+              onClick={() => setIsExpanded(true)}
+              className="my-2 px-4 py-2 border-slate-700 text-slate-700 font-medium text-sm rounded-lg shadow hover:border-slate-700 hover:shadow-lg transition duration-300"
+              type="button"
+            >
+              ראה עוד
+            </button>
+            {/* <button
+              onClick={calculateRoute}
+              className="my-2 px-4 py-2 border-blue-600 text-blue-700 font-medium text-sm rounded-lg shadow hover:border-blue-700 hover:shadow-lg transition duration-300"
+            >
+              חישוב מסלול וזמן הליכה
+            </button> */}
+          </>
         )}
-        <button
-          onClick={calculateRoute}
-          className="my-2 px-4 py-2 border-blue-600 text-blue-700 font-medium text-sm rounded-lg shadow hover:border-blue-700 hover:shadow-lg transition duration-300"
-        >
-          חישוב מסלול וזמן הליכה
-        </button>
       </div>
 
       <div className="mb-2 border border-black rounded-xl">
@@ -139,7 +147,7 @@ const CardMap: React.FC<CardMapProps> = ({
         <div className="mt-auto px-4 pb-4 pt-0 flex justify-end">
           <div
             className="fixed inset-0 bg-black bg-opacity-50 z-40"
-            // onClick={() => setIsExpanded(false)} // סגירה בלחיצה מחוץ לפופאפ
+          // onClick={() => setIsExpanded(false)} // סגירה בלחיצה מחוץ לפופאפ
           >
             {route && (
               <PopUpRoute
@@ -157,338 +165,3 @@ const CardMap: React.FC<CardMapProps> = ({
 
 export default CardMap;
 
-// import React, { useState } from "react";
-// import { GoogleMap, DirectionsRenderer } from "@react-google-maps/api";
-// import PopUpRoute from "./PopUpRoute";
-// import { Types } from "mongoose";
-// import { CardMapProps } from "../types/props/CardMapProps";
-
-// const CardMap: React.FC<CardMapProps> = ({
-//   points = [], // נותנים ערך ברירת מחדל ריק למערך
-//   route,
-//   expanded = false,
-// }) => {
-//   const [directions, setDirections] =
-//     useState<google.maps.DirectionsResult | null>(null);
-
-//   // משתני זמן כסטייט
-//   const [hours, setHours] = useState<number>(0);
-//   const [minutes, setMinutes] = useState<number>(0);
-//   //לפופאפ
-//   const [isExpanded, setIsExpanded] = useState<boolean>();
-
-//   // הגדרת סגנון המפה
-//   const mapContainerStyle = { inlineSize: "100%", blockSize: "250px" };
-//   // בדיקת אם המערך של נקודות ציון ריק
-//   const center = points.length > 0 ? points[0] : { lat: 0, lng: 0 };
-
-//   // פונקציה לקביעת המיקום הנוכחי של המשתמש
-//   // const getCurrentLocation = () => {
-//   //   if (navigator.geolocation) {
-//   //     navigator.geolocation.getCurrentPosition(
-//   //       (position) => {
-//   //         const { latitude, longitude } = position.coords;
-//   //         const userLocation = { lat: latitude, lng: longitude };
-
-//   //         // הוספת המיקום הנוכחי לנקודות
-//   //         points.unshift(userLocation); // הוספת כנקודת מוצא
-//   //         calculateRoute(userLocation); // חישוב המסלול עם המיקום הנוכחי
-//   //       },
-//   //       () => {
-//   //         alert("לא ניתן לקבוע את המיקום שלך.");
-//   //       }
-//   //     );
-//   //   } else {
-//   //     alert("הדפדפן שלך לא תומך במיקום גיאוגרפי.");
-//   //   }
-//   // };
-
-//   const getCurrentLocation = () => {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//           const { latitude, longitude } = position.coords;
-//           const userLocation = { lat: latitude, lng: longitude };
-
-//           // הוספת המיקום הנוכחי לנקודות
-//           points.unshift(userLocation); // הוספת כנקודת מוצא
-//           calculateRoute(userLocation); // חישוב המסלול עם המיקום הנוכחי
-//         },
-//         () => {
-//           alert("לא ניתן לקבוע את המיקום שלך.");
-//         },
-//         {
-//           enableHighAccuracy: true, // דרישה לדיוק גבוה יותר
-//           timeout: 10000,            // הזמן המרבי לחכות עד שהמיקום יתקבל
-//           maximumAge: 0              // לא להשתמש במיקום ישן
-//         }
-//       );
-//     } else {
-//       alert("הדפדפן שלך לא תומך במיקום גיאוגרפי.");
-//     }
-//   };
-
-//   const calculateRoute = (userLocation: google.maps.LatLngLiteral) => {
-//     // אם אין מספיק נקודות למסלול
-//     if (points.length < 2) {
-//       alert("עליך לבחור לפחות שתי נקודות למסלול.");
-//       return;
-//     }
-
-//     const directionsService = new google.maps.DirectionsService();
-//     const request: google.maps.DirectionsRequest = {
-//       origin: userLocation, // התחל מהמיקום של המשתמש
-//       destination: points[points.length - 1], // סיום בנקודת היעד האחרונה במסלול
-//       waypoints: points.slice(1, -1).map((point) => ({
-//         location: point,
-//         stopover: true,
-//       })),
-//       travelMode: google.maps.TravelMode.WALKING,
-//     };
-
-//     directionsService.route(request, (result, status) => {
-//       if (status === google.maps.DirectionsStatus.OK && result) {
-//         calculateWalkingTime(result);
-//         setDirections(result);
-//       } else {
-//         alert("לא ניתן לחשב מסלול.");
-//       }
-//     });
-//   };
-
-//   const calculateWalkingTime = (result: google.maps.DirectionsResult) => {
-//     let totalTimeInSeconds = 0;
-
-//     const route = result.routes[0];
-
-//     route.legs.forEach((leg) => {
-//       totalTimeInSeconds += leg.duration!.value;
-//     });
-
-//     const calculatedHours = Math.floor(totalTimeInSeconds / 3600); // שעות
-//     const calculatedMinutes = Math.floor((totalTimeInSeconds % 3600) / 60); // דקות
-
-//     setHours(calculatedHours);
-//     setMinutes(calculatedMinutes);
-//   };
-
-//   return (
-//     <>
-//       <div className="flex justify-center">
-//         <button
-//           onClick={getCurrentLocation} // השתמש בפונקציה כדי לקבל את המיקום הנוכחי
-//           className="mb-4  px-4 py-2 bg-blue-600 text-white font-medium text-sm rounded-lg shadow hover:bg-blue-700 hover:shadow-lg transition duration-300"
-//         >
-//           חישוב מסלול וזמן הליכה
-//         </button>
-//       </div>
-
-//       <GoogleMap
-//         mapContainerStyle={mapContainerStyle}
-//         center={center} // אם אין נקודות, המפה תתמקד ב־{ lat: 0, lng: 0 }
-//         zoom={12}
-//       >
-//         {directions && (
-//           <DirectionsRenderer
-//             directions={directions}
-//             options={{
-//               polylineOptions: {
-//                 strokeColor: "#FF0000",
-//                 strokeOpacity: 0.8,
-//                 strokeWeight: 2,
-//               },
-//             }}
-//           />
-//         )}
-//       </GoogleMap>
-//       <p>
-//         הזמן הכולל להליכה: {hours} שעות, {minutes} דקות
-//       </p>
-//       <div className="mt-auto px-4 pb-4 pt-0 flex justify-end">
-//         {!isExpanded && !expanded && (
-//           <button
-//             onClick={() => setIsExpanded(true)}
-//             className="mt-auto rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-//             type="button"
-//           >
-//             +
-//           </button>
-//         )}
-//         {isExpanded && (
-//           <div
-//             className="fixed inset-0 bg-black bg-opacity-50 z-40"
-//             // onClick={() => setIsExpanded(false)} // סגירה בלחיצה מחוץ לפופאפ
-//           >
-//             {route && (
-//               <PopUpRoute
-//                 onClose={() => setIsExpanded(false)}
-//                 routeId={route._id as Types.ObjectId}
-//               />
-//             )}
-//           </div>
-//         )}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default CardMap;
-
-// קוד מעודכן עם הוספת מיקום נוכחי למסלול בתנאי שהוא בטווח של קילומטר
-
-// import React, { useState } from "react";
-// import { GoogleMap, DirectionsRenderer } from "@react-google-maps/api";
-// import PopUpRoute from "./PopUpRoute";
-// import { Types } from "mongoose";
-// import { CardMapProps } from "../types/props/CardMapProps";
-
-// const CardMap: React.FC<CardMapProps> = ({
-//   points = [],
-//   route,
-//   expanded = false,
-// }) => {
-//   const [directions, setDirections] =
-//     useState<google.maps.DirectionsResult | null>(null);
-
-//   const [hours, setHours] = useState<number>(0);
-//   const [minutes, setMinutes] = useState<number>(0);
-//   const [isExpanded, setIsExpanded] = useState<boolean>();
-
-//   const mapContainerStyle = { inlineSize: "100%", blockSize: "250px" };
-//   const center = points.length > 0 ? points[0] : { lat: 0, lng: 0 };
-
-//   const getCurrentLocation = () => {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//           const { latitude, longitude } = position.coords;
-//           const userLocation = { lat: latitude, lng: longitude };
-
-//           // נוודא שהמרחק לא יותר מקילומטר אחד
-//           const distance = google.maps.geometry.spherical.computeDistanceBetween(
-//             new google.maps.LatLng(userLocation.lat, userLocation.lng),
-//             new google.maps.LatLng(points[0].lat, points[0].lng)
-//           );
-
-//           if (distance > 1000) {
-//             alert("המרחק מהמסלול רחוק מידי (מעל 1 קילומטר). לא ניתן לבחור את המסלול.");
-//           } else {
-//             points.unshift(userLocation); // הוספת כנקודת מוצא
-//             calculateRoute(userLocation); // חישוב המסלול עם המיקום הנוכחי
-//           }
-//         },
-//         () => {
-//           alert("לא ניתן לקבוע את המיקום שלך.");
-//         },
-//         {
-//           enableHighAccuracy: true,
-//           timeout: 10000,
-//           maximumAge: 0,
-//         }
-//       );
-//     } else {
-//       alert("הדפדפן שלך לא תומך במיקום גיאוגרפי.");
-//     }
-//   };
-
-//   const calculateRoute = (userLocation: google.maps.LatLngLiteral) => {
-//     if (points.length < 2) {
-//       alert("עליך לבחור לפחות שתי נקודות למסלול.");
-//       return;
-//     }
-
-//     const directionsService = new google.maps.DirectionsService();
-//     const request: google.maps.DirectionsRequest = {
-//       origin: userLocation,
-//       destination: points[points.length - 1],
-//       waypoints: points.slice(1, -1).map((point) => ({
-//         location: point,
-//         stopover: true,
-//       })),
-//       travelMode: google.maps.TravelMode.WALKING,
-//     };
-
-//     directionsService.route(request, (result, status) => {
-//       if (status === google.maps.DirectionsStatus.OK && result) {
-//         calculateWalkingTime(result);
-//         setDirections(result);
-//       } else {
-//         alert("לא ניתן לחשב מסלול.");
-//       }
-//     });
-//   };
-
-//   const calculateWalkingTime = (result: google.maps.DirectionsResult) => {
-//     let totalTimeInSeconds = 0;
-
-//     const route = result.routes[0];
-
-//     route.legs.forEach((leg) => {
-//       totalTimeInSeconds += leg.duration!.value;
-//     });
-
-//     const calculatedHours = Math.floor(totalTimeInSeconds / 3600);
-//     const calculatedMinutes = Math.floor((totalTimeInSeconds % 3600) / 60);
-
-//     setHours(calculatedHours);
-//     setMinutes(calculatedMinutes);
-//   };
-
-//   return (
-//     <>
-//       <div className="flex justify-center">
-//         <button
-//           onClick={getCurrentLocation}
-//           className="mb-4  px-4 py-2 bg-blue-600 text-white font-medium text-sm rounded-lg shadow hover:bg-blue-700 hover:shadow-lg transition duration-300"
-//         >
-//           חישוב מסלול וזמן הליכה
-//         </button>
-//       </div>
-
-//       <GoogleMap
-//         mapContainerStyle={mapContainerStyle}
-//         center={center}
-//         zoom={12}
-//       >
-//         {directions && (
-//           <DirectionsRenderer
-//             directions={directions}
-//             options={{
-//               polylineOptions: {
-//                 strokeColor: "#FF0000",
-//                 strokeOpacity: 0.8,
-//                 strokeWeight: 2,
-//               },
-//             }}
-//           />
-//         )}
-//       </GoogleMap>
-//       <p>
-//         הזמן הכולל להליכה: {hours} שעות, {minutes} דקות
-//       </p>
-//       <div className="mt-auto px-4 pb-4 pt-0 flex justify-end">
-//         {!isExpanded && !expanded && (
-//           <button
-//             onClick={() => setIsExpanded(true)}
-//             className="mt-auto rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-//             type="button"
-//           >
-//             +
-//           </button>
-//         )}
-//         {isExpanded && (
-//           <div className="fixed inset-0 bg-black bg-opacity-50 z-40">
-//             {route && (
-//               <PopUpRoute
-//                 onClose={() => setIsExpanded(false)}
-//                 routeId={route._id as Types.ObjectId}
-//               />
-//             )}
-//           </div>
-//         )}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default CardMap;
--- src/app/components/PopUpRoute.tsx
+++ src/app/components/PopUpRoute.tsx
@@ -1,3 +1,95 @@
+// "use client"
+// import React, { useEffect, useState } from "react";
+// import CardMap from "./CardMap";
+// import Star from "./Star";
+// import Image from 'next/image';
+// import CloudinaryUploader from "./CloudinaryUploader";
+// import { editRoutes } from "../services/routeService";
+// import IRoute from "../types/routes";
+// import { fetchRouteById } from "../functions/routesFunctions";
+// import { PopUpRouteProps } from "../types/props/PopUpRouteProps";
+
+// const PopUpRoute: React.FC<PopUpRouteProps> = ({ onClose, routeId, filtered }) => {
+//   const [pictures, setPictures] = useState<string[]>([]);
+//   const [route, setRoute] = useState<IRoute>();
+//   const [isVisible, setIsVisible] = useState<boolean>(false);
+
+//   useEffect(() => {
+//     async function fetchRoute() {
+//       const newRoute = await fetchRouteById(routeId.toString());
+
+//       if (newRoute) {
+//         setRoute(newRoute);
+//         setPictures(newRoute.gallery);
+//         setIsVisible(true); // הצגת הפופ-אפ עם אנימציה
+//       }
+//     }
+//     fetchRoute();
+//   }, []);
+
+//   useEffect(() => {
+//     async function func() {
+//       if (route && pictures && pictures.length > route.gallery.length) {
+//         const response = await editRoutes(routeId.toString(), undefined, pictures);
+//         setRoute(response);
+//         setPictures(response.gallery);
+//       }
+//     }
+//     func();
+//   }, [pictures]);
+
+//   const handleClose = () => {
+//     setIsVisible(false);
+//     setTimeout(() => {
+//       onClose(); // סוגר את הפופ-אפ לאחר האנימציה
+//     }, 800);
+//   };
+
+//   return (
+//     <div
+//       className={`fixed top-0 right-0 h-full w-1/2 bg-white shadow-lg p-6 overflow-y-auto z-50 transition-transform duration-[800ms] transform ${isVisible ? "translate-x-0" : "translate-x-full"}`}
+//     >
+//       <button
+//         onClick={handleClose}
+//         className="absolute top-4 right-4 bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
+//       >
+//         X
+//       </button>
+//       <h2 className="text-2xl font-bold mb-4">פרטי מסלול</h2>
+//       {!route ? (
+//         <p>Loading...</p>
+//       ) : (
+//         <>
+//           <CardMap points={route.pointsArray} route={route} expanded={true} filtered={filtered}/>
+//           <Star rate={route.rate} filtered={1} onClick={() => console.log("pres")} />
+//           <p>{route.description}</p>
+//           {pictures && pictures.length > 0 && (
+//             <div className="flex flex-wrap gap-4 items-center">
+//               {pictures.map((image: string, index: number) => (
+//                 <div key={index} className="flex-shrink-0">
+//                   <Image
+//                     className="rounded-md"
+//                     src={image}
+//                     height={100}
+//                     width={100}
+//                     alt={`Image ${index}`}
+//                     priority
+//                   />
+//                 </div>
+//               ))}
+//             </div>
+//           )}
+//           {filtered !== 1 &&
+//             <CloudinaryUploader setPictures={setPictures} />
+//           }
+//         </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default PopUpRoute;
+
 "use client"
 import React, { useEffect, useState } from "react";
 import CardMap from "./CardMap";
@@ -9,10 +101,13 @@ import IRoute from "../types/routes";
 import { fetchRouteById } from "../functions/routesFunctions";
 import { PopUpRouteProps } from "../types/props/PopUpRouteProps";
 
-
 const PopUpRoute: React.FC<PopUpRouteProps> = ({ onClose, routeId, filtered }) => {
   const [pictures, setPictures] = useState<string[]>([]);
   const [route, setRoute] = useState<IRoute>();
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
+
   useEffect(() => {
     async function fetchRoute() {
       const newRoute = await fetchRouteById(routeId.toString());
@@ -20,14 +115,15 @@ const PopUpRoute: React.FC<PopUpRouteProps> = ({ onClose, routeId, filtered }) =
       if (newRoute) {
         setRoute(newRoute);
         setPictures(newRoute.gallery);
+        setIsVisible(true);
       }
     }
     fetchRoute();
   }, []);
+
   useEffect(() => {
     async function func() {
       if (route && pictures && pictures.length > route.gallery.length) {
-        // Edit route when pictures are updated
         const response = await editRoutes(routeId.toString(), undefined, pictures);
         setRoute(response);
         setPictures(response.gallery);
@@ -36,36 +132,66 @@ const PopUpRoute: React.FC<PopUpRouteProps> = ({ onClose, routeId, filtered }) =
     func();
   }, [pictures]);
 
+  useEffect(() => {
+    if (!isHovered && pictures.length > 1) {
+      const interval = setInterval(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % pictures.length);
+      }, 1300);
+      return () => clearInterval(interval);
+    }
+  }, [isHovered, pictures.length]);
+
+  const handleClose = () => {
+    setIsVisible(false);
+    setTimeout(() => {
+      onClose();
+    }, 500);
+  };
+
+  console.log(routeId);
+  
   return (
-    <div className="fixed top-0 left-0 h-full w-1/2 bg-white shadow-lg p-6 overflow-y-auto z-50 transition-transform transform translate-x-0">
+    <div
+      className={`fixed top-0 right-0 h-full w-1/3 bg-white shadow-lg p-6 overflow-y-auto z-50 transition-transform duration-[500ms] transform ${isVisible ? "translate-x-0" : "translate-x-full"}`}
+    >
       <button
-        onClick={onClose}
+        onClick={handleClose}
         className="absolute top-4 right-4 bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
       >
         X
       </button>
       <h2 className="text-2xl font-bold mb-4">פרטי מסלול</h2>
       {!route ? (
-        <p>Loading...</p> // טקסט טעינה כדי למנוע תקלות
+        <p>Loading...</p>
       ) : (
         <>
           <CardMap points={route.pointsArray} route={route} expanded={true} filtered={filtered}/>
           <Star rate={route.rate} filtered={1} onClick={() => console.log("pres")} />
           <p>{route.description}</p>
           {pictures && pictures.length > 0 && (
-            <div className="flex flex-wrap gap-4 items-center">
-              {pictures.map((image: string, index: number) => (
-                <div key={index} className="flex-shrink-0">
-                  <Image
-                    className="rounded-md"
-                    src={image}
-                    height={100}
-                    width={100}
-                    alt={`Image ${index}`}
-                    priority
+            <div className="flex flex-col items-center">
+              <div 
+                className="relative w-[300px] h-[200px] overflow-hidden"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+              >
+                <Image
+                  className="rounded-md object-cover w-[300px] h-[200px]"
+                  src={pictures[currentImageIndex]}
+                  height={200}
+                  width={300}
+                  alt={`Image ${currentImageIndex}`}
+                  priority
+                />
+              </div>
+              <div className="flex mt-2">
+                {pictures.map((_, index) => (
+                  <div
+                    key={index}
+                    className={`w-3 h-3 mx-1 rounded-full ${index === currentImageIndex ? 'bg-blue-500' : 'bg-gray-400'}`}
                   />
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           )}
           {filtered !== 1 &&
