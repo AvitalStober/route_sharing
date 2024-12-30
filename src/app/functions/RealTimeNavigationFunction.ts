7a29154a5faa705984c5b5e90ee7edee4758f935
YaelNagar <165303327+YaelNagar@users.noreply.github.com>
2024-12-30 10:49:45 +0200

    add home page

--- src/app/functions/RealTimeNavigationFunction.ts
+++ src/app/functions/RealTimeNavigationFunction.ts
@@ -1,4 +1,4 @@
-import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+// import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 // import { calculateDistance } from "./routesFunctions";
 
 export const startNavigation = (
